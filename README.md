23e9ceca90c32883a0fce69d3c8a76368eb47e90
Joao Pires <mail@joaopires.dev>
2022-01-03 13:37:20 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PortugalTrains
+
+A description of this package.
---
+++ Sources/PortugalTrains/PortugalTrains.swift
@@ -0,0 +1,6 @@
+public struct PortugalTrains {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
