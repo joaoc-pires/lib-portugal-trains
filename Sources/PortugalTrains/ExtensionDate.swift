83fb07b0bf34820f842d61c2d393a647f2e2dc21
Joao Pires <mail@joaopires.dev>
2022-01-11 00:09:26 +0000

    Update ExtensionDate.swift

--- Sources/PortugalTrains/ExtensionDate.swift
+++ Sources/PortugalTrains/ExtensionDate.swift
@@ -11,13 +11,13 @@ extension Date {
     
     /// Returns the date in a string formated as yyyy-MM-dd HH:mm
     /// - Note: Infraestruturas de Portugal doesn't use ISO 8601 (NP EN 286010) but rather a close approximation that uses a space to differenciate between day and time.
-    var ipFormated: String {
+    public var ipFormated: String {
         
         let components = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: self)
         return "\(components.year ?? 2022)-\(components.month ?? 1)-\(components.day ?? 1)%20\(components.hour ?? 1):\(components.minute ?? 0)"
     }
     
-    var ipFormatedDay: String {
+    public var ipFormatedDay: String {
         
         let components = Calendar.current.dateComponents([.year, .month, .day], from: self)
         return "\(components.year ?? 2022)-\(components.month ?? 1)-\(components.day ?? 1)"
